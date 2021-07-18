bf1a1efe948cac014381646fccee9db4ea579ddd
0xffset <rene.gottschalk02@gmail.com>
2021-07-18 22:26:18 +0200

    working stack frames

--- 0xVM/src/cpu/cpu_struct.rs
+++ 0xVM/src/cpu/cpu_struct.rs
@@ -4,31 +4,24 @@ use crate::memory::{Byte, Memory, Word};
 
 use super::instruction_codes;
 
-
 pub struct CPU {
-    pub memory: Memory,
+    memory: Memory,
     register_names: Vec<&'static str>,
     registers: Memory,
-    pub register_map: HashMap<&'static str, Byte>,
+    register_map: HashMap<&'static str, Byte>,
+    stackframe_size: Word,
     halt_signal: bool,
 }
 
 impl CPU {
     pub fn new(memory: Memory) -> CPU {
         let register_names = vec![
-            "r1",
-            "r2",
-            "r3",
-            "r4",
-            "r5",
-            "r6",
-            "r7",
-            "r8",
-            "pc", // program counter
+            "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8",  // general purpose registers
+            "pc",  // program counter
             "acc", // accumulator
-            "sr", // status register
-            "sp", // stack pointer
-            "fp", // frame pointer
+            "sr",  // status register
+            "sp",  // stack pointer
+            "fp",  // frame pointer
         ];
         let register_names_len = register_names.len();
 
@@ -43,6 +36,7 @@ impl CPU {
             register_names,
             registers: Memory::new(register_names_len * 4),
             register_map,
+            stackframe_size: 0,
             halt_signal: false,
         };
 
@@ -73,13 +67,11 @@ impl CPU {
     }
 
     /// Gets the value of the register with the given name.
-    fn get_register_by_name(&self, name: &'static str) -> Word {
+    fn get_register_by_name(&self, name: impl AsRef<str>) -> Word {
         self.registers.get_word(
-            *self
-                .register_map
-                .get(&name)
-                .expect(format!("[CPU] get_register: No such register '{}'", name).as_str())
-                as Word,
+            *self.register_map.get(name.as_ref()).expect(
+                format!("[CPU] get_register: No such register '{}'", name.as_ref()).as_str(),
+            ) as Word,
         )
     }
 
@@ -89,23 +81,23 @@ impl CPU {
     }
 
     /// Sets the value of the register with the given name.
-    fn set_register_by_name(&mut self, name: &'static str, value: Word) {
+    fn set_register_by_name(&mut self, name: impl AsRef<str>, value: Word) {
         self.registers.set_word(
-            *self
-                .register_map
-                .get(&name)
-                .expect(format!("[CPU] set_register: No such register '{}'", name).as_str())
-                as Word,
+            *self.register_map.get(name.as_ref()).expect(
+                format!("[CPU] set_register: No such register '{}'", name.as_ref()).as_str(),
+            ) as Word,
             value,
         );
     }
 
     fn get_program_counter(&self) -> Word {
-        self.registers.get_word(*self.register_map.get("pc").unwrap() as Word)
+        self.registers
+            .get_word(*self.register_map.get("pc").unwrap() as Word)
     }
 
     fn set_program_counter(&mut self, value: Word) {
-        self.registers.set_word(*self.register_map.get("pc").unwrap() as Word, value);
+        self.registers
+            .set_word(*self.register_map.get("pc").unwrap() as Word, value);
     }
 
     /// Sets the value of the register with the given address.
@@ -124,7 +116,7 @@ impl CPU {
     /// Fetches the next word from memory and increments the program counter.
     fn fetch_word(&mut self) -> Word {
         let next_instruction_address = self.get_program_counter();
-        self.set_program_counter( next_instruction_address + 4);
+        self.set_program_counter(next_instruction_address + 4);
 
         self.memory.get_word(next_instruction_address)
     }
@@ -133,12 +125,52 @@ impl CPU {
         let sp_address = self.get_register_by_name("sp");
         self.memory.set_word(sp_address, value);
         self.set_register_by_name("sp", sp_address - 4);
+
+        self.stackframe_size += 4;
     }
 
     fn pop(&mut self) -> Word {
         let next_sp_address = self.get_register_by_name("sp") + 4;
         self.set_register_by_name("sp", next_sp_address);
+
+        self.stackframe_size -= 4;
+
         return self.memory.get_word(next_sp_address);
+    }
+
+    fn push_state(&mut self) {
+        for i in 1..9 {
+            self.push(self.get_register_by_name(format!("r{}", i)));
+        }
+
+        self.push(self.get_program_counter());
+        self.push(self.stackframe_size + 4);
+
+        self.set_register_by_name("fp", self.get_register_by_name("sp"));
+        self.stackframe_size = 0;
+    }
+
+    fn pop_state(&mut self) {
+        let fp_address = self.get_register_by_name("fp");
+        self.set_register_by_name("sp", fp_address);
+
+        let stackframe_size = self.pop();
+        self.stackframe_size = stackframe_size;
+
+        let pc_address = self.pop();
+        self.set_program_counter(pc_address);
+
+        for i in (1..9).rev() {
+            let gp_register_value = self.pop();
+            self.set_register_by_name(format!("r{}", i), gp_register_value);
+        }
+
+        let arg_count = self.pop();
+        for _ in 0..arg_count {
+            self.pop();
+        }
+
+        self.set_register_by_name("fp", fp_address + stackframe_size);
     }
 
     fn execute(&mut self, instruction: Byte) {
@@ -195,7 +227,7 @@ impl CPU {
             // PUSH 0x0000 1234 -> Push 0x0000 1234 onto stack
             instruction_codes::PUSH => {
                 let value = self.fetch_word();
-                
+
                 self.push(value);
             }
             // PUSHR r1 -> Push register r1 onto stack
@@ -236,7 +268,7 @@ impl CPU {
                 let flag = self.fetch_byte();
                 let address = self.fetch_word();
                 if self.get_status_flag(flag) {
-                    self.set_program_counter( address);
+                    self.set_program_counter(address);
                 }
             }
             // BRBC FLAG_Z, 0x0000 00AF -> If the flag Z is clear, jump to 0x0000 00AF
@@ -244,16 +276,16 @@ impl CPU {
                 let flag = self.fetch_byte();
                 let address = self.fetch_word();
                 if !self.get_status_flag(flag) {
-                    self.set_program_counter( address);
+                    self.set_program_counter(address);
                 }
             }
-            // BREQ 0x0000 1234, 0x0000 0005 -> Jump to 0x0000 0005 if add does equal 0x0000 1234
+            // BREQ 0x0000 1234, 0x0000 0005 -> Jump to 0x0000 0005 if acc does equal 0x0000 1234
             instruction_codes::BREQ => {
                 let value = self.fetch_word();
                 let address = self.fetch_word();
 
                 if self.get_register_by_name("acc") == value {
-                    self.set_program_counter( address);
+                    self.set_program_counter(address);
                 }
             }
             // BRNQ 0x0000 1234, 0x0000 0005 -> Jump to 0x0000 0005 if acc does not equal 0x0000 1234
@@ -262,8 +294,29 @@ impl CPU {
                 let address = self.fetch_word();
 
                 if self.get_register_by_name("acc") != value {
-                    self.set_program_counter( address);
+                    self.set_program_counter(address);
                 }
+            }
+            // CALL 0x0000 00AF -> Call subroutine at 0x0000 00AF
+            instruction_codes::CALL => {
+                let address = self.fetch_word();
+
+                self.push_state();
+
+                self.set_program_counter(address);
+            }
+            // CALL r1 -> Call subroutine at r1
+            instruction_codes::CALLR => {
+                let register_address = self.fetch_byte();
+                let address = self.get_register_by_address(register_address);
+
+                self.push_state();
+
+                self.set_program_counter(address);
+            }
+            // RET -> Return from subroutine
+            instruction_codes::RET => {
+                self.pop_state();
             }
             _ => {
                 panic!("[CPU] No such instruction: '0x{:02X}'", instruction);
@@ -271,18 +324,14 @@ impl CPU {
         }
     }
 
-    pub fn debug(&self) {
+    fn debug(&self) {
         for name in self.register_names.clone() {
-            println!(
-                "{:<4}: 0x{:08X}",
-                name,
-                self.get_register_by_name(name)
-            );
+            println!("{:<4}: 0x{:08X}", name, self.get_register_by_name(name));
         }
         println!();
     }
 
-    pub fn view_memory_at(&self, address: Word, n: Word) {
+    fn view_memory_at(&self, address: Word, n: Word) {
         let mut mem_snapshot: Vec<Byte> = Vec::new();
         let max_address = if address + n < self.memory.get_size() {
             address + n
@@ -304,8 +353,26 @@ impl CPU {
     }
 
     /// Progresses the program
-    pub fn step(&mut self) {
+    fn step(&mut self) {
         let instruction = self.fetch_byte();
         self.execute(instruction);
+    }
+
+    pub fn run_debug(&mut self, address: Word, n: Word) {
+        use std::io;
+
+        while !self.halt_signal {
+            self.step();
+            self.debug();
+            self.view_memory_at(address, n);
+
+            io::stdin().read_line(&mut String::new()).unwrap();
+        }
+    }
+
+    pub fn run(&mut self) {
+        while !self.halt_signal {
+            self.step();
+        }
     }
 }
--- 0xVM/src/main.rs
+++ 0xVM/src/main.rs
@@ -1,83 +1,77 @@
 #![feature(panic_info_message)]
 
 mod memory;
-use std::panic;
+use std::{collections::HashMap, panic};
 
 use memory::Memory;
 mod cpu;
 use cpu::{instruction_codes, CPU};
 
+use crate::memory::Byte;
+
 fn main() {
     panic::set_hook(Box::new(|panic_info| {
-		if let Some(s) = panic_info.message() {
-			println!("0xVM panicked at\n{}", s);
-		} else if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+        if let Some(s) = panic_info.message() {
+            println!("0xVM panicked at\n{}", s);
+        } else if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
             println!("0xVM panicked at\n{}", s);
         } else {
             println!("0xVM panicked!");
         }
     }));
 
-    
-    let memory = Memory::new(0xFF);
+    let register_names = vec![
+        "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8",  // general purpose registers
+        "pc",  // program counter
+        "acc", // accumulator
+        "sr",  // status register
+        "sp",  // stack pointer
+        "fp",  // frame pointer
+    ];
+
+    let register_map: HashMap<&'static str, Byte> = register_names
+        .iter()
+        .enumerate()
+        .map(|(i, n)| (n.clone(), i as Byte * 4))
+        .collect();
+
+    let mut memory = Memory::new(0xFFFF);
+
+    memory.set_byte(0, instruction_codes::PUSH);
+    memory.set_word(1, 0x33333335);
+    memory.set_byte(5, instruction_codes::PUSH);
+    memory.set_word(6, 0x22222225);
+    memory.set_byte(10, instruction_codes::PUSH);
+    memory.set_word(11, 0x11111115);
+    memory.set_byte(15, instruction_codes::MOVR);
+    memory.set_word(16, 0x12341235);
+    memory.set_byte(20, *register_map.get("r1").unwrap());
+    memory.set_byte(21, instruction_codes::MOVR);
+    memory.set_word(22, 0x56785675);
+    memory.set_byte(26, *register_map.get("r4").unwrap());
+    memory.set_byte(27, instruction_codes::PUSH);
+    memory.set_word(28, 0x00000000);
+    memory.set_byte(32, instruction_codes::CALL);
+    memory.set_word(33, 3000);
+    memory.set_byte(37, instruction_codes::PUSH);
+    memory.set_word(38, 0x44444445);
+    memory.set_byte(42, instruction_codes::HALT);
+
+    memory.set_byte(3000, instruction_codes::PUSH);
+    memory.set_word(3001, 0x01020105);
+    memory.set_byte(3005, instruction_codes::PUSH);
+    memory.set_word(3006, 0x03040305);
+    memory.set_byte(3010, instruction_codes::PUSH);
+    memory.set_word(3011, 0x05060505);
+    memory.set_byte(3015, instruction_codes::MOVR);
+    memory.set_word(3016, 0x07080705);
+    memory.set_byte(3020, *register_map.get("r1").unwrap());
+    memory.set_byte(3021, instruction_codes::MOVR);
+    memory.set_word(3022, 0x080A0805);
+    memory.set_byte(3026, *register_map.get("r8").unwrap());
+    memory.set_byte(3027, instruction_codes::RET);
+
+
     let mut cpu = CPU::new(memory);
-
-    cpu.memory.set_byte(0, instruction_codes::MOVR);
-    cpu.memory.set_word(1, 0x12345678);
-    cpu.memory.set_byte(5, *cpu.register_map.get("r1").unwrap());
-
-    cpu.memory.set_byte(6, instruction_codes::PUSH);
-    cpu.memory.set_word(7, 0x90ABCDEF);
-    cpu.memory.set_byte(11, instruction_codes::PUSHR);
-    cpu.memory.set_byte(12, *cpu.register_map.get("r1").unwrap());
-
-    cpu.memory.set_byte(13, instruction_codes::POP);
-    cpu.memory.set_byte(14, *cpu.register_map.get("r3").unwrap());
-
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.view_memory_at(0xF0, 0xFF)
-
-    /*
-    cpu.memory.set_byte(0, instruction_codes::MOVR);
-    cpu.memory.set_word(1, 0x1234);
-    cpu.memory.set_byte(5, *cpu.register_map.get("r1").unwrap());
-
-    cpu.memory.set_byte(6, instruction_codes::MOVR);
-    cpu.memory.set_word(7, 0xABCD);
-    cpu.memory
-        .set_byte(11, *cpu.register_map.get("r2").unwrap());
-
-    cpu.memory.set_byte(12, instruction_codes::ADDR);
-    cpu.memory
-        .set_byte(13, *cpu.register_map.get("r1").unwrap());
-    cpu.memory.set_byte(14, *cpu.register_map.get("r2").unwrap());
-
-    cpu.memory.set_byte(15, instruction_codes::MOVRM);
-    cpu.memory
-        .set_byte(16, *cpu.register_map.get("acc").unwrap());
-    cpu.memory.set_word(17, 0x3A);
-	cpu.memory.set_byte(21, instruction_codes::HALT);
-
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-    cpu.step();
-    cpu.debug();
-	cpu.step();
-	cpu.debug();
-    cpu.view_memory_at(0, 0x4C);
-    */
-
+    cpu.run_debug(0xffff-1-84, 0xffff);
 }
